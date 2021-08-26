182fcf570a6625ec08f3f6e2b70e7b530a4af98c
PradeepHore123 <pradeephore2@gmail.com>
2021-08-26 05:11:04 +0000

    jsr 303 validation

--- src/main/java/com/tcs/springbootdemo/User.java
+++ src/main/java/com/tcs/springbootdemo/User.java
@@ -4,12 +4,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@Email
+	private String email;
+	@NotBlank
 	private String firstName;
 
 	public String getFirstName() {
